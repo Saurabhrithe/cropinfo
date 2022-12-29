2aacc0f003d23e47a07f5a9a07c68afefa6efdd6
Saurabhrithe <58046438+Saurabhrithe@users.noreply.github.com>
2022-12-29 16:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,31 @@
-
 from flask import Flask, render_template, request
-import requests
 import pickle
-import pandas as pd
-
-
+import preprocessing as pre
 
 fName1 = 'modelCr.pkl'
 modelCr = pickle.load(open(fName1, 'rb'))
 
 app = Flask(__name__)
 
+
 @app.route('/call_W')
 def call_W():
     return render_template('weather.html')
 
+
 @app.route('/get_W', methods=['POST'])
 def get_W():
-    df = pd.read_csv( 'Weather.csv', parse_dates=['Time'], index_col=['Time'] )
-    # print("1", df)
     place = request.form['place']
-    place_data = df[df['Place'] == place].drop( ['Month'], axis=1 )
-    # print("2", place_data)
-    l = list( set( df['Place'] ) )
-    a, di = range( 1, len( l ) ), {}
-    for k, v in zip( a, l ):
-        di[v] = k
-    # print(di)
-    df['Place'] = df['Place'].replace( di )
-    # print("3", place)
-    place_data = round(place_data.resample('M').mean(), 2)
+    freq = str(request.form['options'])
 
-    # print( "4", place_data )
+    l, val3 = pre.load(place, freq)
+    values1, values2, values3 = l[0], l[1], val3
 
-    labels = [
-        'JAN', 'FEB', 'MAR', 'APR',
-        'MAY', 'JUN', 'JUL', 'AUG',
-        'SEP', 'OCT', 'NOV', 'DEC'
-    ]
-    values1 = list(place_data['Humidity'])
-    values2 = list( place_data['Temperature'] )
-    values3 = list(place_data['Precipitation'])
-    #mx = max(max(values1), max(values2), max(values3))
+    mx = round(max(max(values1), max(values2), max(values3)))
+    t_min, t_max, feels_like, h = pre.live_weather(place)
+    return render_template('weather.html', labels=l[2], values1=values1, values2=values2, values3=values3, place=place,
+                            temp_min=t_min, temp_max=t_max, feels_like=feels_like, humidity=h, mx=mx)
 
-
-
-    return render_template('weather.html', labels=labels, values1=values1, values2=values2, values3=values3, place=place)
 
 @app.route('/')
 def index():
@@ -55,30 +35,6 @@ def index():
 @app.route('/call_weather')
 def call_weather():
     return render_template('index.html')
-
-
-@app.route('/get_weather', methods=['POST'])
-def get_weather():
-    cName = str(request.form['City'])
-    api_key = "516b385972ffc07a66437095d39ab15d"
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    complete_url = base_url + "appid=" + api_key + "&q=" + cName
-
-    response = requests.get(complete_url)
-    x = response.json()
-
-    if x["cod"] != "404":
-        y = x["main"]
-
-        temp = round( (y["temp"] - 273.15), 2)
-        humidity = y["humidity"]
-
-
-        return render_template('index.html', predictionT=temp, predictionH=humidity)
-    else:
-        return None
-
-
 
 
 @app.route('/predict', methods=['POST'])
@@ -93,14 +49,11 @@ def predict():
 
     inputsCr = [nitrogen, phosporous, potassium, temperature, humidity, phValue, rainfall]
     crop_suggestion = int(modelCr.predict([inputsCr]))
-    
 
     label = ['apple', 'banana', 'blackgram', 'chickpea', 'coconut', 'coffee', 'cotton',
              'grapes', 'jute', 'kidneybeans', 'lentil', 'maize', 'mango', 'mothbeans',
              'mungbean', 'muskmelon', 'orange', 'papaya', 'pigeonpeas', 'pomegranate',
              'rice', 'watermelon']
-    # print(label[soil_fertility-1])
-
 
     return render_template('index.html', predictionCr=label[crop_suggestion-1])
 
